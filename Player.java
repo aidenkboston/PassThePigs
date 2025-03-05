c6f95697e52890514849dbdc22983014a2fc9788
aidenkboston <acrott@brooksschool.org>
2025-03-05 01:43:38 +0000

    Initial setup

---
+++ Player.java
@@ -0,0 +1,16 @@
+public class Player {
+    private String name;
+    private String strategy;
+    public Player(String name) {
+        name = this.name; 
+    }
+    public String getName() {
+        retrun name;
+    }
+    public String getStrategy() {
+        return strategy;
+    }
+    public boolean wantsToRoll(int myScore, int handScore, ArrayList<Integer> otherScores, int winningScore) {
+
+    }    
+}
